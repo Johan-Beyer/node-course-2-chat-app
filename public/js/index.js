efa2243347377dd1999fc71d3b5ee3350ceb6136
Johan Beyer <johanbeyer09@gmail.com>
2018-04-02 17:29:16 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-  
-  socket.emit('createMessage', {
-    from: 'Johan',
-    text: 'Hey this is Johan.'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', function (socket) {
   console.log('New user connected');
   
-  socket.emit('newMessage', {
-    from: 'Piet',
-    text: 'Hey. What is going on.',
-    createdAt: 123
-  });
-  
   socket.on('createMessage', function (message) {
     console.log('createMesage', message);
+    io.emit('newMessage', {
+      from:message.from,
+      text:message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', function (socket) {
